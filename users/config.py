ad573dd6765916d5eb497c793b6f6dc844787b83
a.ukhanov <alexander-ukhanov@yandex.ru>
2025-03-08 00:59:13 +0300

    HW-2 Fixed runtime

--- users/config.py
+++ users/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:postgres@postgres:5432/user_service')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:postgres@localhost:5432/user_service')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'your_super_secret_key_change_in_production')
     JWT_ACCESS_TOKEN_EXPIRES = 3600
